9de21e0d4bcd858f36f1fbae91225558347a7c60
Alif Mohd-Amir <alifma.96@gmail.com>
2023-08-12 15:54:46 -0400

    update go plugin

--- .config/nvim/lua/plugins/go.lua
+++ .config/nvim/lua/plugins/go.lua
@@ -101,4 +101,22 @@ return {
       end
     end,
   },
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "nvim-neotest/neotest-go",
+    },
+    opts = {
+      adapters = {
+        ["neotest-go"] = {
+          -- Here we can set options for neotest-go, e.g.
+          -- args = { "-tags=integration" }
+        },
+      },
+    },
+  },
+  {
+    "nvim-neotest/neotest-go",
+  },
 }

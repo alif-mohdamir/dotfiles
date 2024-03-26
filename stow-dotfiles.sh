45551813c226271023e1651262163dd96c5fc3a4
Alif Mohd-Amir <alifma.96@gmail.com>
2024-03-26 08:23:56 -0400

    Finish implementing stow

--- stow-dotfiles.sh
+++ stow-dotfiles.sh
@@ -6,8 +6,9 @@ pushd $DOTFILES
 # Loop through each folder to stow
 for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g"); do
 	echo "stow $folder"
-	sow -D $folder
-	stow $folder
+	# stow creates the symlink to the parent directory of where the command is run if no --target is specified
+	stow -D --target ~ $folder
+	stow --target ~ $folder
 done
 
 # Change directory back to the previous location

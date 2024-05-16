e379c2414975309357de17f84edc7d75a98908fa
Alif Mohd-Amir <78119758+alif-mohdamir@users.noreply.github.com>
2024-05-16 12:51:18 -0400

    update nvim-kickstart

--- nvim-kickstart/.config/nvim-kickstart/lua/kickstart/plugins/lspconfig.lua
+++ nvim-kickstart/.config/nvim-kickstart/lua/kickstart/plugins/lspconfig.lua
@@ -146,8 +146,8 @@ return {
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
+        gopls = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -155,7 +155,7 @@ return {
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
+        tsserver = {},
         --
 
         lua_ls = {

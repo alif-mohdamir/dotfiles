da7365208eb914f46569314a0770fddaacc307b5
Alif Mohd-Amir <alif.mohdamir@agilebits.com>
2024-10-30 21:48:44 -0400

    tsserver replaced with ts_ls

--- nvim-kickstart/.config/nvim-kickstart/lua/kickstart/plugins/lspconfig.lua
+++ nvim-kickstart/.config/nvim-kickstart/lua/kickstart/plugins/lspconfig.lua
@@ -9,11 +9,11 @@ return {
 
       -- Useful status updates for LSP.
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
+      { 'j-hui/fidget.nvim',       opts = {} },
 
       -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
       -- used for completion, annotations and signatures of Neovim apis
-      { 'folke/neodev.nvim', opts = {} },
+      { 'folke/neodev.nvim',       opts = {} },
     },
     config = function()
       -- Brief aside: **What is LSP?**
@@ -155,7 +155,8 @@ return {
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
+        -- now ts_ls?
+        ts_ls = {},
         --
 
         lua_ls = {

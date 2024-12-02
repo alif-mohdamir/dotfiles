904b06eeca68d40ffe886273d6e2d30cab8a3d01
Alif Mohd-Amir <78119758+alif-mohdamir@users.noreply.github.com>
2024-12-02 08:00:46 -0500

    Add sessionizer shortcut and update paste behavior in v mode

--- nvim/.config/nvim/lua/kickstart/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'vim', 'vimdoc', 'go', 'gomod', 'gowork', 'gosum' },
+      ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'go', 'gomod', 'gowork', 'gosum' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {

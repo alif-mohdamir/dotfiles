35b7c166de8c8e8c6ef425d014c1cc8f8eb8186c
Alif Mohd-Amir <78119758+alif-mohdamir@users.noreply.github.com>
2025-01-02 17:27:33 -0500

    update gopls to use build tags for test files

--- nvim/.config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -148,7 +148,13 @@ return {
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        gopls = {},
+        gopls = {
+          settings = {
+            gopls = {
+              buildFlags = { '-tags=debug' },
+            },
+          },
+        },
         pyright = {},
         ['terraform-ls'] = {},
         -- rust_analyzer = {},

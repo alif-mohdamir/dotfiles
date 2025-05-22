028ef0983926f911fc5e4ed73af426101ee85f57
Alif Mohd-Amir <78119758+alif-mohdamir@users.noreply.github.com>
2025-05-22 11:13:22 -0400

    More fugitive keymaps

--- nvim/.config/nvim/lua/custom/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/custom/plugins/fugitive.lua
@@ -7,9 +7,10 @@ return {
     set('n', '<leader>gl', ':Git difftool --name-status --merge-base origin/main <CR>', { desc = 'Git diff list' })
     set('n', '<leader>gd', ':Gvdiff origin/main<CR>', { desc = 'Git diff vertical split' })
     set('n', '<leader>gprom', ':G pull --rebase origin main', { desc = 'Git pull --rebase origin main' })
-    set('n', '<leader>gA', ':G add .<CR>', { desc = '[G]it add [A]LL modified files' })
+    set('n', '<leader>gaA', ':G add .<CR>', { desc = '[G]it [a]dd [A]LL modified files' })
     set('n', '<leader>ga', ':G add %<CR>', { desc = '[G]it add current modified file' })
-    set('n', '<leader>gc', ':G commit -a<CR>', { desc = 'Git commit' })
+    set('n', '<leader>gc', ':G commit <CR>', { desc = 'Git commit staged files' })
+    set('n', '<leader>gcA', ':G commit -a<CR>', { desc = '[G]it [c]ommit and stage [A]LL files' })
     -- vim.keymaps.set('n', '<leader>gs', vim.cmd.Git)
     --
     --     local autocmd = vim.api.nvim_create_autocmd

4cda1bb631eb71a014dee6d1c6e202c80927002a
Alif Mohd-Amir <78119758+alif-mohdamir@users.noreply.github.com>
2025-09-10 18:12:39 -0400

    feat:truncate branch in status line

--- nvim/.config/nvim/lua/kickstart/plugins/mini.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/mini.lua
@@ -22,7 +22,35 @@ return {
       --  and try some other statusline plugin
       local statusline = require 'mini.statusline'
       -- set use_icons to true if you have a Nerd Font
-      statusline.setup { use_icons = vim.g.have_nerd_font }
+      statusline.setup {
+        use_icons = vim.g.have_nerd_font,
+        -- active = function()
+        --   local mode, mode_hl = statusline.section_mode { trunc_width = 120 }
+        --   local git = statusline.section_git { trunc_width = 10 }
+        --   local diff = statusline.section_diff { trunc_width = 75 }
+        --   local diagnostics = statusline.section_diagnostics { trunc_width = 75 }
+        --   local lsp = statusline.section_lsp { trunc_width = 75 }
+        --   local filename = statusline.section_filename { trunc_width = 140 }
+        --   local fileinfo = statusline.section_fileinfo { trunc_width = 120 }
+        --   local search = statusline.section_searchcount { trunc_width = 75 }
+        --   -- Truncate git branch name to 15 chars
+        --   if git ~= '' then
+        --     git = git:gsub(' (%S+)', function(branch)
+        --       return ' ' .. branch:sub(1, 15)
+        --     end)
+        --   end
+        --
+        --   return statusline.combine_groups {
+        --     { hl = mode_hl, strings = { mode } },
+        --     { hl = 'MiniStatuslineDevinfo', strings = { git, diff, diagnostics, lsp } },
+        --     '%<', -- Mark general truncate point
+        --     { hl = 'MiniStatuslineFilename', strings = { filename } },
+        --     '%=', -- End left alignment
+        --     { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+        --     { hl = mode_hl, strings = { search } },
+        --   }
+        -- end,
+      }
 
       -- You can configure sections in the statusline by overriding their
       -- default behavior. For example, here we set the section for
@@ -30,6 +58,27 @@ return {
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
         return '%2l:%-2v'
+      end
+
+      local orig = statusline.section_git
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_git = function(args)
+        local s = orig(args)
+        if s == '' then
+          return s
+        end
+        -- take the last non-space chunk as the branch; keep the original prefix
+        local branch = s:match '(%S+)$'
+        if not branch then
+          return s
+        end
+        local prefix = s:sub(1, #s - #branch)
+
+        -- truncate to 15 chars (add … if you like)
+        if #branch > 15 then
+          branch = branch:sub(1, 15) -- or: branch = branch:sub(1, 15) .. "…"
+        end
+        return prefix .. branch
       end
 
       -- ... and there is more!
